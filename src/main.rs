14cc51c6c0360da784febcd674e2d6462a1bbd3b
Jordy Moos <jordymoos@gmail.com>
2018-05-30 23:17:45 +0200

    Trying to display a square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+extern crate piston;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use sdl2_window::Sdl2Window as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+pub struct App {
+    gl: GlGraphics,
+    rotation: f64,
 }
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = ((args.width / 2) as f64,
+                      (args.height / 2) as f64);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(GREEN, gl);
+
+            let transform = c.transform.trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+
+            rectangle(RED, square, transform, gl);
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        self.rotation += 2.0 * args.dt;
+    }
+}
+
+fn main() {
+    let opengl = OpenGL::V2_1;
+
+    let mut window: Window = WindowSettings::new(
+            "ping-game",
+            [200, 200]
+        )
+        .opengl(opengl)
+        .vsync(true)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
+    }
+}
+
+
